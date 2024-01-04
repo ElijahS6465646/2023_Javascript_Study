37fdffe70387b95059fb99e68c2d2abcace35cba
Elijah S <elijah.suarez@vtsd.com>
2024-01-04 11:43:40 -0500

    Update

---
+++ Calculator/calc.js
@@ -0,0 +1,29 @@
+/// Setup Access to HTML Elements ///
+const buttons = document.querySelectorAll('button');
+const display = document.querySelector('.display');
+/// Add Event Listeners to the Buttons ///
+buttons.forEach(
+    function(button){
+        button.addEventListener('click', calculate);
+    }
+);
+/// Calculate Function - event handler for all buttons///
+function calculate(event){
+    ///What button was clicked?///
+    const clickedValue = event.target.value;
+
+    ///Check if = sign was clicked///
+    if(clickedValue === '='){
+        ///Is the display is empty?///
+        if (display.value !== ''){
+            ///Display the value in the top of the calculator///
+            display.value = eval(display.value);
+        }
+    }else if(clickedValue === 'C'){
+        ///Clear the display///
+        display.value = '';
+    }else{
+        ///Concatenate the clicked value to the display///
+        display.value += clickedValue;
+    }
+}
