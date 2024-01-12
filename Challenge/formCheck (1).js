60c6084212f5afd5f60ca6b7a6f22504b51d94da
Elijah S <elijah.suarez@vtsd.com>
2024-01-12 13:39:12 -0500

    Update

--- Challenge/formCheck (1).js
+++ Challenge/formCheck (1).js
@@ -2,8 +2,6 @@
 const submitButton = document.getElementById('submit');
 const nameField = document.getElementById('name');
 const emailField = document.getElementById('email');
-const phoneField = document.getElementById('phone');
-
 // Add event listener to submit button
 submitButton.addEventListener('click', checkForm);
 
